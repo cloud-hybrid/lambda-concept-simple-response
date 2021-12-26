9e89e34bd10133df46ab60092d9651e59253605e
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-12-25 21:38:33 -0600

    [Development] - Update

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const XRay = require("aws-xray-sdk-core");
+const AWS = XRay.captureAWS(require("aws-sdk"));
+
+// Create Client outside of Handler for reuse
+const Lambda = new AWS.Lambda();
+
+/*** Handler (Async Handler(s) *should* return a non-awaited promise */
+exports.handler = async function (event, context) {
+    event.Records.forEach(record => {
+        console.log(record.body);
+    });
+
+    console.log("[Debug] Environment Variable(s)" + ":", serialize(process.env));
+    console.log("[Debug] Lambda Handler Context" + ":", serialize(context));
+    console.log("[Debug] Event" + ":", serialize(event));
+
+    return getAccountSettings();
+};
+
+// Use SDK Client
+const getAccountSettings = function () {
+    return Lambda.getAccountSettings().promise();
+};
+
+const serialize = (object) => JSON.stringify(object, null, 4);
