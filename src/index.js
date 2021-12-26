eed285c67789627eef1595cda8ecbb5042c2d5c9
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-12-26 07:46:28 -0600

    [Development] - Response

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ exports.handler = async function (event, context) {
     console.log("[Debug] Lambda Handler Context" + ":", serialize(context));
     console.log("[Debug] Event" + ":", serialize(event));
 
-    return serialize({
-        "Layer-Example": Layer
+    return new Promise((resolve) => {
+        resolve({
+            statusCode: 200,
+            headers: {
+                "Content-Type": "Application/JSON",
+            },
+            body: JSON.stringify({
+                Message: Layer
+            }, null, 4)
+        });
     });
 };
 
 const serialize = (object) => JSON.stringify(object, null, 4);
+
